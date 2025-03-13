4a29a925bde58f554ac04c747c6103ce80563836
tim.sadko@gmail.com <tim.sadko@gmail.com>
2025-03-13 10:33:35 +0100

    Add project files.

---
+++ SomerenWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SomerenWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
