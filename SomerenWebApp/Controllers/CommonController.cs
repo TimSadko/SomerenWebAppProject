dcf2f61a8f7f50895bd8735caff0881752ac6db0
zeanoox <zenoox2003@gmail.com>
2025-03-27 13:31:15 +0100

    Added Participants

---
+++ SomerenWebApp/Controllers/CommonController.cs
@@ -0,0 +1,14 @@
+﻿using SomerenWebApp.Repositories;
+
+namespace SomerenWebApp.Controllers
+{
+	public static class CommonController
+	{
+		public static IActivityRepository _activity_rep;
+		public static IStudentRepositorie _student_rep;
+		public static ILecturerRepositorie _lecturer_rep;
+		public static IRoomRepository _room_rep;
+		public static ISupervisorReposiory _supervisor_rep;
+		public static IParticipantsRepository _participant_rep;
+	}
+}
